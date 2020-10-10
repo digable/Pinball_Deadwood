1ec10e00f3972bb2a67ef2cf43222a82be929121
miguelcerca <63877305+miguelcerca@users.noreply.github.com>
2020-10-10 17:51:06 -0500

    Add files via upload

---
+++ Startup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xterm -e "cd ~/pinball/deadwood && mpf both"
---
+++ github.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf ~/pinball
+
+git clone https://github.com/digable/Pinball_Deadwood --depth 1 --branch=master ~/pinball/deadwood
+
